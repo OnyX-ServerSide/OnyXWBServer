f3537f2702636b26e9e1c553316c813a403a40ac
ServerSide <p93751406@gmail.com>
2026-03-24 23:36:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-app.post('/register_game', (req, res) => {
-    try {
-        const data = JSON.parse(req.body);
-        activeGames[data.gameName] = {
-            author: data.author,
-            placeId: data.placeId,
-            playerCount: data.playerCount,
-            maxPlayers: data.maxPlayers,
-            status: data.status,
-            lastSeen: Date.now()
-        };
-        res.send("Registered");
-    } catch(e) { res.status(400).send("Error"); }
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8080;
+
+let scripts = {};
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.get('/key/:id', (req, res) => {
+    const gameId = req.params.id;
+    const script = scripts[gameId] || '';
+    res.send(script);
+});
+
+app.post('/key/:id', (req, res) => {
+    const gameId = req.params.id;
+    const script = req.body.script;
+
+    if (!gameId) {
+        res.status(400).send('No Game ID Specified');
+        return;
+    }
+
+    scripts[gameId] = script;
+    res.send('Succesfully Executed');
+});
+
+app.listen(port, () => {
+    console.log('Warning Vision will be entirely rewritten soon from scratch please make sure to keep your repository updated to the latest version.')
+    console.log(`Server is listening on port ${port}`);
 });
